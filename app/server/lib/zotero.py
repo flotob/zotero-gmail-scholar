29c6d6fed1b0f5faaf61bbbafa21c28c97803e12
Florian <florian@glatz-mahlberg.de>
2015-06-07 15:37:56 +0200

    working file upload implementation through js-python bridge to pyzotero

--- app/server/lib/zotero.py
+++ app/server/lib/zotero.py
@@ -1,29 +1,13 @@
 import sys, json
 from pyzotero import zotero
 
-# zot = zotero.Zotero(library_id, library_type, api_key)
-
-
-
 for msg in sys.stdin:
     args = json.loads(msg)
 
     if args['subject'] == 'config':
-      print 'config'
+      zot = zotero.Zotero(args['user'], args['lib'], args['key'])
+      print '{ "status": "success", "msg": "configured" }'
 
     if args['subject'] == 'upload':
-      print 'upload'
-
-# zot = zotero.Zotero(library_id, library_type, api_key)
-# items = zot.top(limit=5)
-# # we've retrieved the latest five top-level items in our library
-# # we can print each item's item type and ID
-# for item in items:
-#     print('Item: %s | Key: %s') % (item['data']['itemType'], item['data']['key'])
-
-# for line in sys.stdin:
-#     print line;
-
-# text_file = open("debug.log", "w")
-# text_file.write()
-# text_file.close()
+      ul = zot.attachment_simple(args['files'])
+      print '{ "status": "success", "msg": ' + json.dumps(ul) + ' }'
--- app/server/tests/fileupload.js
+++ app/server/tests/fileupload.js
@@ -3,49 +3,78 @@ var jf = require('jsonfile');
 
 var config = jf.readFileSync('config/config.json');
 
-var file = '/var/folders/2j/5tfdfmhs2x10t_kgnhkbwgcc0000gn/T/printing.751.1/Creating PDF Workflow Options.pdf';
+var files = ['/ripple.pdf'];
 
 var PythonShell = require('python-shell');
 
-var io = (function () {
-  var pyshell = new PythonShell('zotero.py', { 
-    scriptPath: './lib',
-    pythonOptions: ['-u']
-  });
-  
-  var send = function (subject, body) {
-    pyshell.send( JSON.stringify(_.extend(body, { subject:subject } )));
-  };
-
-  var listen = function (callback) {
-    pyshell.on('message', callback);
-  };
-
-  return {
-    pub: send,
-    sub: listen,
-    kill: pyshell.end
-  }
-})();
-
-io.pub('config', { credentials: config.zotero.credentials });
-
-io.sub(function (msg) {
-  // received a message sent from the Python script (a simple "print" statement)
-  if (msg == 'config') {
-    io.kill(function (err) {
-      if (err) throw err;
-      console.log('finished1');
-    });
-  }
+var pyshell = new PythonShell('zotero.py', {
+  scriptPath: './lib',
+  pythonOptions: ['-u']
 });
 
+function send (subject, body) {
+  pyshell.send(JSON.stringify(_.extend(body, { subject:subject })));
+}
 
-// backup kill
-io.kill(function (err) {
+// sends a message to the Python script via stdin
+send('config', config.zotero.credentials);
+send('upload', { files:files } ); 
+
+pyshell.on('message', function (raw) {
+  resp = JSON.parse(raw);
+  console.log(resp);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
   if (err) throw err;
-  console.log('finished2');
+  console.log('finished');
 });
+
+
+
+
+
+
+// var io = (function () {
+//   // var pyshell = new PythonShell('zotero.py', { 
+//   //   // scriptPath: '../blubb/lib',
+//   //   // pythonOptions: ['-u']
+//   // });
+  
+//   var send = function (subject, body) {
+//     pyshell.send( JSON.stringify(_.extend(body, { subject:subject } )));
+//   };
+
+//   var listen = function (callback) {
+//     pyshell.on('message', callback);
+//   };
+
+//   return {
+//     pub: send,
+//     sub: listen,
+//     kill: pyshell.end
+//   }
+// })();
+
+// io.pub('config', { credentials: config.zotero.credentials });
+
+// io.sub(function (msg) {
+//   // received a message sent from the Python script (a simple "print" statement)
+//   if (msg == 'config') {
+//     io.kill(function (err) {
+//       if (err) throw err;
+//       console.log('finished1');
+//     });
+//   }
+// });
+
+
+// // backup kill
+// io.kill(function (err) {
+//   if (err) throw err;
+//   console.log('finished2');
+// });
 
 
 // PythonShell.run('zotero.py', {
