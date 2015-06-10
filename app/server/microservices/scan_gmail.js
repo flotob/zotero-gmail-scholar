8edb2bdd44e9ac193ff63b5a6eff33a1c11e2f60
Florian <florian@glatz-mahlberg.de>
2015-06-10 20:26:54 +0200

    added first jobs

---
+++ app/server/microservices/scan_gmail.js
@@ -0,0 +1,34 @@
+var _ = require('lodash');
+var jf = require('jsonfile');
+var path = require('path');
+
+var kue = require('kue')
+  , queue = kue.createQueue();
+
+var zotero = require('./zotero');
+var gmail = require('./gmail');
+var download = require('./download');
+
+var config = jf.readFileSync('config/config.json');
+var collections = config.zotero.collections;
+
+gmail.getItems(_.keys(collections))
+  .on('item', function (item, keyword) {
+    var clean_url = download.cleanURL(item.url);
+    var pdf = download.isPDF(clean_url);
+
+    // set zotero template type, assume we're dealing with articles bc of google scholar
+    _.extend(item, { 
+      itemType: config.zotero.template,
+      collections: collections[keyword],
+      url: clean_url
+    });
+
+    var job = queue.create('zotero/create', {
+      item: item,
+      download: pdf ? true : false
+    }).save( function(err){
+      if( !err ) console.log('error on creation of job: ', job );
+      else console.log('created job: ', job);
+    });
+  });
